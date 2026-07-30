469fe084e5ded01f7279d92e69b3e25f029620ef
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2026-07-30 12:42:28 +0100

    add windows arm64 support to cuda-samples (#5)
    * add windows arm64 support to cuda-samples
    
    * refactor conditional patching

--- samples/cuda-samples/conanfile.py
+++ samples/cuda-samples/conanfile.py
@@ -1,8 +1,9 @@
 import os
+import platform
 
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
-from conan.tools.files import get, patch
+from conan.tools.files import get, patch, rename
 
 
 class cuda_samplesRecipe(ConanFile):
@@ -16,13 +17,25 @@ class cuda_samplesRecipe(ConanFile):
         cuda_samples_url = "https://github.com/NVIDIA/cuda-samples/archive/refs/tags/v13.2update.tar.gz"
         cuda_samples_checksum = "057e68d22bd02e41d60c9826e7622ac1b88de0f1dbe25ed49bd995f768306f9d"
         get(self, cuda_samples_url, sha256=cuda_samples_checksum, strip_root=True)
-        patch_file = os.path.join(self.export_sources_folder, "patches", "13.2-windows-fixes.patch")
-        patch(self, patch_file=patch_file)
+        if platform.system() == "Windows":
+            # Note: conditional patches are not good practice, but this isn't a
+            # packaged recipe. need a more robust approach.
+            for patch_file in ["13.2-windows-fixes.patch", "13.2-windows-fixes-arm64.patch"]:
+                patch(self, patch_file=os.path.join(self.export_sources_folder, "patches", patch_file))
+            # Rename the GL folder and use these from Conan
+            rename(self, 
+               os.path.join(self.source_folder, "Common", "GL"),
+               os.path.join(self.source_folder, "Common", "GL__"))
 
     def requirements(self):
         self.requires("cuda-toolkit/[>=13 <14]")
         self.requires("opengl/system")
         self.requires("freeimage/[*]")
+        if self.settings.os == "Windows":
+            # cuda-samples uses vendored glew binaries that are x86_64 only
+            # lets use them from Conan on Windows to support Windows arm64 too
+            self.requires("glew/[*]")
+            self.requires("freeglut/[*]")
 
     def build_requirements(self):
         self.tool_requires("cuda-toolkit/<host_version>")
